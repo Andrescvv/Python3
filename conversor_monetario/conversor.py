330b01eb28b338aaf67ae3c494c26ef8f7047f6b
Andres Calvo <andrescalvo28073@gmail.com>
2021-01-16 01:37:49 -0600

    Actualizacion del conversor monetario

--- conversor_monetario/conversor.py
+++ conversor_monetario/conversor.py
@@ -1,9 +1,34 @@
-colones = float(input("Cuantos colones tienes?: "))
+def money_Changer(currency, currency_value):
 
-valor_dolar = 611.59
+    currency_amount = float(input("How many " + currency + " do you have?: "))
 
-dolares = (colones / valor_dolar)
-dolares = str(round(dolares, 2))
+    dolars = (currency_amount / currency_value)
+    dolars = str(round(dolars, 2))
 
-print("Tienes $" + dolares + " dolares")
+    print("You have $" + dolars + " dolars")
+    
+    return 
 
+welcome_message = """ 
+❤ Welcome to the currency changer ❤
+Choose one of this three options to convert in dolars
+
+1: Pesos colombianos 
+2: Pesos argentinos 
+3: Colones costarricenses 
+
+Your option: """
+
+chosen_option = int(input(welcome_message))
+
+if chosen_option == 1:
+    money_Changer("pesos colombianos", 3462.11)
+
+elif chosen_option == 2:
+    money_Changer("pesos argentinos", 85.74)
+
+elif chosen_option == 3:
+    money_Changer("colones costarricenses", 611.59)
+
+else:
+    print("Choose a correct number")
